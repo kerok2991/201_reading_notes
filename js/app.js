361afc0e0e2ed4530f32a722d6e6a31f38d2937f
KYRILLOS KHALIL <kerok2991@gmail.com>
2021-10-30 13:21:07 -0500

    added kero's changes

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
 "use strict"
+// Kero's changes
 
-
-let question1 = prompt("What is your name?") 
+function question1 = prompt("What is your name?") 
     alert("Welcome " + question1 + ". I have some questions for you")
         //console.log("What is your name?")
      
 
-let question2 = prompt("Do I like football?").toLowerCase 
+function question2 = prompt("Do I like football?").toLowerCase 
     if (question2 === "yes" || question2 === "y") {
         alert("Incorrect! I don't watch football")
     } else {
         alert("Yep. Don't watch football. What a crime")
 }
 
-let question3 = prompt("Do I like cheese?").toLowerCase;
+function question3 = prompt("Do I like cheese?").toLowerCase;
     if (question3 === "yes" || question3 === "y") {
     alert("I love cheese. Try Lactaid")
 } else {
     "Favorite cheeses are Havarti"
 }
 //console.log("Glad you like cheese. Try Lactaid")
-let question4 = prompt("Have I left the country for vacation?").toLowerCase
+function question4 = prompt("Have I left the country for vacation?").toLowerCase
 if (question4 === "yes" || question4 === "y") {
     alert("Correct! I went to Greece and Italy last summer. It was fun")
 } else {
     alert("Nope, I went to Europe before Covid-19 started.")
 }
 //console.log("Correct! I went to Greece and Italy last summer. It was fun")
-let question5 = prompt("What is my favorite activity?").toLowerCase
+function question5 = prompt("What is my favorite activity?").toLowerCase
 if (question5 === "gaming" || question5 === "playing games") {
     alert("Good answer " + question1 + "!. My favorite right now is New World. Welcome to the website")
 } else {
@@ -35,8 +35,8 @@ if (question5 === "gaming" || question5 === "playing games") {
 }
 guessingGameTries = 5
 function userGuessingGame() {
-    let userLives = 4;
-    let userAnswer;
+    function userLives = 4;
+    function userAnswer;
     while (userLives > 0) {
         userAnswer = prompt("Guess a number between 1 and 100");
         if (userAnswer < 47) {
@@ -58,10 +58,10 @@ function userGuessingGame() {
 
 function guessAnother2ndQuestion() {
 
-    let secondGuessGameTries = 6
-    let secondGuessGameCorrectAnswer = ["interstellar", "avatar", "john wick"]
+    function secondGuessGameTries = 6
+    function secondGuessGameCorrectAnswer = ["interstellar", "avatar", "john wick"]
     while (secondGuessGameTries > 0) {
-        let secondGameUseranswer = prompt("What is one of my favorite movies?").toLowerCase()
+        function secondGameUseranswer = prompt("What is one of my favorite movies?").toLowerCase()
         if (secondGameUseranswer != secondGuessGameCorrectAnswer) {
             alert("That is not a correct answer please try again")
         } else (secondGameUseranswer === secondGuessGameCorrectAnswer) 
@@ -73,6 +73,9 @@ function guessAnother2ndQuestion() {
 
 guessingGameTries()
 guessAnother2ndQuestion()
-
-
-
+question1();
+question2();
+question3();
+question4();
+question5();
+userGuessingGame();
